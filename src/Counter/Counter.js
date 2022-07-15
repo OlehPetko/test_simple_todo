930db11e523c4aada3481211aed4601b31bcdba0
OLLO77 <petkooleg@gmail.com>
2022-07-15 11:00:17 +0100

    new counter

--- src/Counter/Counter.js
+++ src/Counter/Counter.js
@@ -2,16 +2,31 @@ import '../App.css';
 import {useState} from "react";
 
 function Counter() {
-    const [count, setCount] = useState(0)
-    const minusPlusCounter = (valueCounter) => {
-      setCount(count + valueCounter)
+    const initialState = [
+
+    ]
+    const [count, setCount] = useState(initialState)
+    const minusPlusCounter = (id, valueCounter) => {
+     const newCount = count.map(el => el.id === id ? {...el, counter: el.counter + valueCounter} : el)
+        setCount(newCount)
+    }
+    const addCounter = () => {
+ const newCount = [...count, {id: Math.random(), counter: 0}]
+        setCount(newCount)
     }
   return (
     <div className="App">
         <h1>COUNTER</h1>
-      <button onClick={() => minusPlusCounter(- 1)}>-</button>
-        {count}
-      <button onClick={() => minusPlusCounter(1)}>+</button>
+        <button onClick={addCounter}>add counter</button>
+
+        {count.map(el =>
+        <div key={el.id}>
+            <button onClick={() => minusPlusCounter(el.id, - 1)}>-</button>
+            {el.counter}
+            <button onClick={() => minusPlusCounter(el.id, 1)}>+</button>
+        </div>
+        )}
+
 
     </div>
   );
